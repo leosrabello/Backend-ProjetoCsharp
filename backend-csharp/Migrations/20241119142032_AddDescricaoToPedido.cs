90e29b492ec1651a75b5732cd890e708244e607d
leosrabello <122031549+leosrabello@users.noreply.github.com>
2024-11-26 14:23:43 -0300

    commit backend 1

---
+++ backend-csharp/Migrations/20241119142032_AddDescricaoToPedido.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace teste2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescricaoToPedido : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Pedidos",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Pedidos");
+        }
+    }
+}
---
+++ backend-csharp/README.md
@@ -0,0 +1,14 @@
+## Biblioteca ##
+
+APIs: Livro - Cliente - Autores - Pedidos - Pagamentos
+
+
+Livro
+- add um livro
+- editar info de um livro
+- excluir um livro
+- listar todos os livros 
+
+get post put delete
+create read update delete
+
